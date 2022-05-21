12e5789a486410e763aa47acf4a30cf9559c057f
Armando Arenas <armandoarenass@hotmail.com>
2022-05-20 23:55:41 -0500

    Implementación enviar datos

--- pedidos/views.py
+++ pedidos/views.py
@@ -4,43 +4,24 @@ from django.utils import timezone
 from .models import Menu
 
 
-# def listMenus(request):
-#     menu = Menu.objects.all()
 
-#     context = {
-#         'name': Menu.name
-#     }
-
-#     return render(request, 'pedidos/menu.html', context)
 
 def index(request):
-    return render(request, 'index.html', {})
+    if request.method=="POST":
+        message_name= request.POST['message_name']
+        message_address= request.POST['message_address']
+        message_cellphone= request.POST['message_cellphone']
+        message_product= request.POST['message_product']
+    else:
+        return render(request, 'index.html', {})
 
 
 def menu(request):
     return render(request, 'menu.html', {})
-
 
 def about(request):
     return render(request, 'about.html', {})
 
 def book(request):
     return render(request, 'book.html', {})
-
-
-
-
-
-
-
-
-
-
-
-# class IndexView(generic.ListView):
-#     template_name= 'pedidos/index.html'
-    
-#     def get_queryset(self):
-#         return Pedido.objects.filter(fecha_pedido__lte=timezone.now())
-
 
