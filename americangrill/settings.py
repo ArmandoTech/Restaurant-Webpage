c5c5009360ef755168faf43ff5b36f28780e3b47
Armando Arenas <armandoarenass@hotmail.com>
2022-05-22 23:29:15 -0500

    Envíos al correo funcionando (CON CONTRASEÑA)

--- americangrill/settings.py
+++ americangrill/settings.py
@@ -124,6 +124,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_HOST_USER= 'americangrillbga@gmail.com'
-EMAIL_HOST_PASSWORD= '' 
+EMAIL_HOST_PASSWORD= 'american1423' 
 EMAIL_USE_TLS= True
 #EMAIL_USE_SSL= False
