d4e323c6f26fad69f2c2e529da632435152230b0
Armando Arenas <armandoarenass@hotmail.com>
2022-05-22 23:28:14 -0500

    Envíos funcionando del correo eléctronico (sin contraseña)

--- americangrill/settings.py
+++ americangrill/settings.py
@@ -116,6 +116,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS= [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+#Email settings
+# myaccount.google.com/lesssecureapps
+# https://accounts.google.com/DisplayUnlockCatpcha
+# myaccount.google.com/apppasswords
+
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_HOST_USER= 'americangrillbga@gmail.com'
+EMAIL_HOST_PASSWORD= '' 
+EMAIL_USE_TLS= True
+#EMAIL_USE_SSL= False
--- pedidos/views.py
+++ pedidos/views.py
@@ -13,13 +13,16 @@ def index(request):
         message_address= request.POST['message_address']
         message_cellphone= request.POST['message_cellphone']
         message_product= request.POST['message_product']
+        message_email= request.POST['message_email']
+        message_date= request.POST['message_date']
+
+        final_message=f'DIRECCION: {message_address}, CELULAR: {message_cellphone}, PRODUCTO: {message_product}, FECHA: {message_date}'
 
         send_mail(
             'NUEVO PEDIDO DE: ' + message_name,
-            {'Dirección' : message_address,
-            'Celular': message_cellphone,
-            'Pedido': message_product},
-            ['armandoarenass@hotmail.com']
+            final_message,
+            message_email,
+            ['americangrillbga@gmail.com']
             )
 
         return render(request, 'index.html', {'message_name': message_name})
@@ -34,5 +37,24 @@ def about(request):
     return render(request, 'about.html', {})
 
 def book(request):
-    return render(request, 'book.html', {})
+    if request.method=="POST":
+        message_name= request.POST['message_name']
+        message_address= request.POST['message_address']
+        message_cellphone= request.POST['message_cellphone']
+        message_product= request.POST['message_product']
+        message_email= request.POST['message_email']
+        message_date= request.POST['message_date']
+
+        final_message=f'DIRECCION: {message_address}, CELULAR: {message_cellphone}, PRODUCTO: {message_product}, FECHA: {message_date}'
+
+        send_mail(
+            'NUEVO PEDIDO DE: ' + message_name,
+            final_message,
+            message_email,
+            ['americangrillbga@gmail.com']
+            )
+
+        return render(request, 'book.html', {'message_name': message_name})
+    else:
+        return render(request, 'book.html', {})
 
