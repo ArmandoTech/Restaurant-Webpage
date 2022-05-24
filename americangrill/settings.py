771fa949ef70b5c59a8e3a123fe0329189720d9b
Armando Arenas <armandoarenass@hotmail.com>
2022-05-24 17:45:29 -0500

    se elimina font-awesome.min.css

--- americangrill/settings.py
+++ americangrill/settings.py
@@ -1,4 +1,4 @@
-
+import os
 import django_heroku
 import dj_database_url
 from decouple import config
@@ -11,7 +11,7 @@ import django
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT= BASE_DIR / 'static'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
